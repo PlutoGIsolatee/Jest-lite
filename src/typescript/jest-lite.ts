8027542412f07d24acc06b811cd59392a071f2e7
PlutoGIsolatee <plutoqweguo@126.com>
2026-02-21 20:10:11 +0800

    2

--- src/typescript/jest-lite.ts
+++ src/typescript/jest-lite.ts
@@ -3,20 +3,7 @@
  * 不是对Jest的完全模拟，而是以更轻量的方式实现主要API
  */
 
-const JestLite = Object.create(null);
-JestLite.logger = console.log;
 
-function test(name: string, fn) {
-    try {
-        fn();
-        JestLite.logger(`${name} 测试成功`);
-    } catch (e) {
-        JestLite.logger(`${name} error.tag 测试失败`);
-        JestLite.logger(e);
-    }
-}
-
-const it = test;
 
 type Tester = (
     this: TesterContext,
@@ -48,7 +35,13 @@ function eq(
     customTesters: Array<Tester>,
     strictCheck: boolean | undefined,
 ): boolean {
+
     let result = true;
+
+    const asymmetricResult = asymmetricMatch(a, b);
+    if (asymmetricResult !== undefined) {
+        return asymmetricResult;
+    }
 
     const testerContext: TesterContext = { equals };
 
@@ -125,6 +118,112 @@ function eq(
     if (isDomNode(a) && isDomNode(b)) {
         return a.isEqualNode(b);
     }
+
+    //开始结构递归遍历判断
+    let length = aStack.length;
+    while (length--) {
+        if (aStack[length] === a) {
+            return bStack[length] === b;
+        } else if (bStack[length] === b) {
+            return false;
+        }
+    }
+    aStack.push(a);
+    bStack.push(b);
+
+    if (strictCheck && className === '[object Array]' && a.length !== b.length) {
+        return false;
+    }
+
+    const aKeys = keys(a, hasOwnKey);
+    let key;
+
+    const bKeys = keys(b, hasOwnKey);
+
+    if (!strictCheck) { 
+        for (let index = 0; index !== bKeys.length; ++index) {
+            key = bKeys[index];
+            if((isAsymmetric(a[key]) || b[key] === undefined) && !hasOwnKey(b, key)){
+                aKeys.push(key);
+            }
+        }
+        for (let index = 0; index !== aKeys.length; ++index) {
+            key = aKeys[index];
+            if((isAsymmetric(b[key]) || a[key] === undefined) && !hasOwnKey(a, key)){
+                bKeys.push(key);
+            }
+        }
+    }
+
+    let size = aKeys.length;
+    if(bKeys.length !== size){
+        return false;
+    }
+
+    while (size--) {
+        key = aKeys[size];
+
+        if(strictCheck){
+            result =hasOwnKey(b, key) && 
+            eq(a[key], b[key], aStack, bStack, customTesters, strictCheck);
+        } else {
+            result = (hasOwnKey(b, key) ||
+            isAsymmetric(a[key]) || a[key] === undefined) &&
+            eq(a[key], b[key], aStack, bStack, customTesters, strictCheck);
+        }
+
+        if (!result) {
+            return false;
+        }
+    }
+
+    aStack.pop();
+    bStack.pop();
+
+    return result;
+}
+
+
+
+function isAsymmetric(obj: any) {
+    return Boolean(obj) && isA('Function', obj.asymmetricMatch);
+}
+
+function asymmetricMatch(a: any, b: any) {
+    const asymmetricA = isAsymmetric(a);
+    const asymmetricB = isAsymmetric(b);
+
+    if (asymmetricA && asymmetricB) {
+        return undefined;
+    } else if (asymmetricA) {
+        return a.asymmetricMatch(b);
+    } else if (asymmetricB) {
+        return b.asymmetricMatch(a);
+    }
+}
+
+
+function keys(obj: object, filterKey: (obj: object, key: string) => boolean) {
+    const keys = [];
+    for (const key in obj) {
+        if (filterKey(obj, key)) {
+            keys.push(key);
+        }
+    }
+    return [
+        ...keys,
+        ...Object.getOwnPropertySymbols(obj).filter(
+            symbol => Object.getOwnPropertyDescriptor(obj, symbol)!.enumerable,
+        ),
+    ];
+}
+
+function hasOwnKey(obj: any, key: string | symbol) {
+    return Object.prototype.hasOwnProperty.call(obj, key);
+}
+
+function isA<T>(typeName: string, value: unknown): value is T {
+    return Object.prototype.toString.call(value) === `[object ${typeName}]`;
 }
 
 function isDomNode(obj: any): boolean {
